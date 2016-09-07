c264440f5a00355415458a525d94966a215e4fc7
naveenkashyap <naveen@thekashyaps.net>
2016-09-06 20:51:17 -0700

    Main Activity and partial navigation drawer implementation

---
+++ Pupper/app/src/main/java/com/programming/pupper/pupper/DrawerItemClickListener.java
@@ -0,0 +1,14 @@
+package com.programming.pupper.pupper;
+
+import android.view.View;
+import android.widget.AdapterView;
+
+/**
+ * Created by naveenkashyap on 9/6/16.
+ */
+public class DrawerItemClickListener implements android.widget.AdapterView.OnItemClickListener {
+    @Override
+    public  void onItemClick(AdapterView<?> parent, View view, int position, long id){
+        //selectItem(position);
+    }
+}
---
+++ Pupper/app/src/main/java/com/programming/pupper/pupper/MainActivity.java
@@ -0,0 +1,73 @@
+package com.programming.pupper.pupper;
+
+import android.app.Activity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends Activity{
+    private String[] mMainDrawerItems;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+
+    private ActionBarDrawerToggle mDrawerToggle;
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTitle = mDrawerTitle = getTitle();
+        mMainDrawerItems = getResources().getStringArray(R.array.main_drawer_items);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, // may need R.drawable.ic_drawer
+                R.string.main_drawer_open, R.string.main_drawer_close){
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getActionBar().setTitle(mTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getActionBar().setTitle(mDrawerTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        // TODO deprecated
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mMainDrawerItems));
+
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+    }
+
+    /* Called whenever we call invalidateOptionsMenu() */
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action items related to the content view
+        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+        menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+}
